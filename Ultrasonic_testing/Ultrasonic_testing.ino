d3168afc2cd742cfe9205ca8a7779240c7baa8d8
Bassam Albakri <Bassam@Bassams-MacBook-Air.local>
2016-03-15 14:16:03 -0400

    code for using ultrasonic range finder

---
+++ Ultrasonic_testing/Ultrasonic_testing.ino
@@ -0,0 +1,37 @@
+const int ci_Ultrasonic_Ping = 2;   //input plug
+const int ci_Ultrasonic_Data = 3;   //output plug
+unsigned long ul_Echo_Time;
+
+void setup() {
+Serial.begin(9600);
+
+// set up ultrasonic
+  pinMode(ci_Ultrasonic_Ping, OUTPUT);
+  pinMode(ci_Ultrasonic_Data, INPUT);
+}
+
+void loop() {
+
+Ping();
+
+}
+
+void Ping()
+{
+  //Ping Ultrasonic
+  //Send the Ultrasonic Range Finder a 10 microsecond pulse per tech spec
+  digitalWrite(ci_Ultrasonic_Ping, HIGH);
+  delayMicroseconds(10);  //The 10 microsecond pause where the pulse in "high"
+  digitalWrite(ci_Ultrasonic_Ping, LOW);
+  //use command pulseIn to listen to Ultrasonic_Data pin to record the
+  //time that it takes from when the Pin goes HIGH until it goes LOW
+  ul_Echo_Time = pulseIn(ci_Ultrasonic_Data, HIGH, 10000);
+
+  // Print Sensor Readings
+  Serial.print("Time (microseconds): ");
+  Serial.print(ul_Echo_Time, DEC);
+  Serial.print(", Inches: ");
+  Serial.print(ul_Echo_Time / 148); //divide time by 148 to get distance in inches
+  Serial.print(", cm: ");
+  Serial.println(ul_Echo_Time / 58); //divide time by 58 to get distance in cm
+}
